fd8660109e64ab57535f74931cde752e4e0cc3cc
toghrulvg <toghrulvg@code.edu.az>
2022-07-04 01:12:04 +0400

    commited

---
+++ power/FindingPower.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power
+{
+    public static class Power
+    {
+        public static int CalculatePower(this int num1, int num2)
+        {
+            int counting = 1;
+           
+            for (int i = 1; i <= num2; i++)
+                counting = counting * num1;
+
+            return counting;
+
+        }
+    }
+}
---
+++ power/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Power
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int n = 13;
+            
+            int m = 3;
+
+            Console.WriteLine(n.CalculatePower(m));
+
+           
+
+
+        }
+
+    }
+}
